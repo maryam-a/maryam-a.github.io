7953ec9c35658e339a6cc33319fba56602382c97
Maryam Archie <marchie@mit.edu>
2017-06-17 03:41:04 -0400

    Add nav bar

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello! This site is under construction. Thank you for your patience'
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello! This site is under construction. Thank you for your patience'
+//   }
+// })
+
+$(document).ready(function () {
+  console.log($("img").height() - 10);
+  $(".overlay").offset({ top: ($("img").height()) * 2 });
+  window.onresize = function (event) {
+    console.log($("img").height() - 10);
+    $(".overlay").offset({ top: ($("img").height()) * 2 });
   }
-})
+});
