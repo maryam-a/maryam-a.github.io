9edb7f122037c02ae679b97d7b3eaf42fc8fb058
Maryam Archie <marchie@mit.edu>
2017-07-04 18:39:56 -0400

    Add headers

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-// var app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Hello! This site is under construction. Thank you for your patience'
-//   }
-// })
-
 $(document).ready(function () {
-  console.log($("img").height() - 10);
-  $(".overlay").offset({ top: ($("img").height()) * 2 });
+  $(".overlay").offset({ top: ($("img").height()) * 2});
+
+
   window.onresize = function (event) {
-    console.log($("img").height() - 10);
     $(".overlay").offset({ top: ($("img").height()) * 2 });
   }
 });
